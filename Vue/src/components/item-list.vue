ee2c4f20491078d040a9dda168cf4c9f4045ac5c
kaatenn <1924198467@qq.com>
2022-11-24 19:57:26 +0800

    网页主题内容

--- Vue/src/components/item-list.vue
+++ Vue/src/components/item-list.vue
@@ -1,10 +1,27 @@
 <template>
-Main
+  <el-table :data="items" stripe>
+    <el-table-column prop="item_id" label="游戏 id"></el-table-column>
+    <el-table-column prop="item_launch_date" label="游戏发行时间"></el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
-  name: "item-list"
+  name: "item-list",
+  data() {
+    return {
+      items: [
+        {
+          item_id: 0,
+          item_launch_date: '2022/01/01'
+        },
+        {
+          item_id: 1,
+          item_launch_date: '2022/02/01'
+        }
+      ]
+    }
+  }
 }
 </script>
 
