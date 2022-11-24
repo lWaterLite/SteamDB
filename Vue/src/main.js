174659c1e90189970a1b11537cd092951730b4e4
kaatenn <1924198467@qq.com>
2022-11-24 10:23:21 +0800

    init

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import installElementPlus from './plugins/element'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.mount('#app')
+
