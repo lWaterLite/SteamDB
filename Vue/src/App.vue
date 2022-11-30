10efb9251ab98951947f944fb58f1e2bdc12a7bf
kaatenn <1924198467@qq.com>
2022-11-30 11:07:04 +0800

    页面基本框架

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header class="el-header" height="100px" style="user-select: none">steam 游戏管理</el-header>
+      <el-header class="el-header" height="100px" style="user-select: none">
+        steam 游戏管理
+        <el-select
+            v-model="value"
+            class="language"
+            placeholder="选择语言"
+            size="small"
+            style="position: absolute; right: 10px; height: 5px; user-select: none">
+          <el-option v-for="item in languages" :key="item.language_id" :label="item.language_label"
+                     :value="item.language_name"></el-option>
+        </el-select>
+      </el-header>
       <el-container>
         <el-aside width="200px" style="height: 300px; user-select: none">
           <el-menu
@@ -9,11 +20,19 @@
               class="el-menu"
           >
             <el-menu-item index="1" style="font-family: serif; font-size: large">
-              <svg height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa=""><path fill="currentColor" d="M224 704h576V318.336L552.512 115.84a64 64 0 0 0-81.024 0L224 318.336V704zm0 64v128h576V768H224zM593.024 66.304l259.2 212.096A32 32 0 0 1 864 303.168V928a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V303.168a32 32 0 0 1 11.712-24.768l259.2-212.096a128 128 0 0 1 162.112 0z"></path><path fill="currentColor" d="M512 448a64 64 0 1 0 0-128 64 64 0 0 0 0 128zm0 64a128 128 0 1 1 0-256 128 128 0 0 1 0 256z"></path></svg>
+              <svg height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa="">
+                <path fill="currentColor"
+                      d="M224 704h576V318.336L552.512 115.84a64 64 0 0 0-81.024 0L224 318.336V704zm0 64v128h576V768H224zM593.024 66.304l259.2 212.096A32 32 0 0 1 864 303.168V928a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V303.168a32 32 0 0 1 11.712-24.768l259.2-212.096a128 128 0 0 1 162.112 0z"></path>
+                <path fill="currentColor"
+                      d="M512 448a64 64 0 1 0 0-128 64 64 0 0 0 0 128zm0 64a128 128 0 1 1 0-256 128 128 0 0 1 0 256z"></path>
+              </svg>
               <span style="margin-left: 20px">游戏列表</span>
             </el-menu-item>
             <el-menu-item index="2" style="font-family: serif; font-size: large">
-              <svg height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa=""><path fill="currentColor" d="m795.904 750.72 124.992 124.928a32 32 0 0 1-45.248 45.248L750.656 795.904a416 416 0 1 1 45.248-45.248zM480 832a352 352 0 1 0 0-704 352 352 0 0 0 0 704zm-32-384v-96a32 32 0 0 1 64 0v96h96a32 32 0 0 1 0 64h-96v96a32 32 0 0 1-64 0v-96h-96a32 32 0 0 1 0-64h96z"></path></svg>
+              <svg height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa="">
+                <path fill="currentColor"
+                      d="m795.904 750.72 124.992 124.928a32 32 0 0 1-45.248 45.248L750.656 795.904a416 416 0 1 1 45.248-45.248zM480 832a352 352 0 1 0 0-704 352 352 0 0 0 0 704zm-32-384v-96a32 32 0 0 1 64 0v96h96a32 32 0 0 1 0 64h-96v96a32 32 0 0 1-64 0v-96h-96a32 32 0 0 1 0-64h96z"></path>
+              </svg>
               <span style="margin-left: 20px">添加游戏</span>
             </el-menu-item>
           </el-menu>
@@ -29,9 +48,33 @@
 
 <script>
 import listPage from "./components/list-page.vue";
+import {ref} from 'vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      value: ref('zh_cn'),
+      languages: [
+        {
+          language_id: 1,
+          language_label: 'English',
+          language_name: 'en'
+        },
+        {
+          language_id: 2,
+          language_label: '简体中文',
+          language_name: 'zh_cn'
+        },
+        {
+          language_id: 3,
+          language_label: '繁体中文',
+          language_name: 'zh_tw'
+        },
+
+      ]
+    }
+  },
   components: {
     listPage
   }
@@ -51,5 +94,4 @@ body {
   font-family: serif;
   line-height: 100px;
 }
-
 </style>
--- Vue/src/components/item-list.vue
+++ Vue/src/components/item-list.vue
@@ -1,10 +1,9 @@
 <template>
-  {{language_id}}
-  <el-table :data="items" stripe @row-click="rowClickHandler">
+  <el-table :data="items" stripe @row-click="rowClickHandler" style="user-select: none">
     <el-table-column prop="item_id" label="游戏 id" width="100px"></el-table-column>
-    <el-table-column prop="item_name" label="游戏名"></el-table-column>
-    <el-table-column prop="publisher" label="发行商"></el-table-column>
-    <el-table-column prop="item_launch_date" label="游戏发行时间" width="200px"></el-table-column>
+    <el-table-column prop="item_launch_date" label="游戏发行时间"></el-table-column>
+    <el-table-column prop="content" label="好评度"></el-table-column>
+    <el-table-column prop="price" label="售价" width="200px"></el-table-column>
   </el-table>
 </template>
 
@@ -12,30 +11,29 @@
 
 export default {
   name: "item-list",
-  props: {
-    language_id: String
-  },
   data() {
     return {
+      detail: [],
       items: [
         {
-          item_id: 0,
+          item_id: 77658,
           item_launch_date: '2022/01/01',
-          item_name: '《使命召唤®：现代战争®II 2022》 | 《战争地带2.0》',
-          publisher: 'Activision'
+          content: '特别好评',
+          price: 98
         },
         {
-          item_id: 1,
+          item_id: 33412,
           item_launch_date: '2022/02/01',
-          item_name: '饥荒联机版',
-          publisher: 'KLei'
+          content: '褒贬参半',
+          price: 48
         }
       ]
     }
   }, //data
   methods: {
-    rowClickHandler(row, event, column) {
+    rowClickHandler(row) {
       console.log(row.item_id)
+      this.$emit('getItem', row.item_id, row)
     }
   }
 }
