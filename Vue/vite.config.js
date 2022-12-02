8792508cea1ab6de3cf141e4413de18b74f07033
kaatenn <1924198467@qq.com>
2022-12-02 21:34:42 +0800

    add proxy

--- Vue/vite.config.js
+++ Vue/vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080,
-    open: true
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/\/api/, '')
+      }
+    }
   }
 })
