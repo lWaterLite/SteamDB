4b6e943bc53982ccfa986f80fe5e6d9636520c17
kaatenn <1924198467@qq.com>
2022-12-03 21:08:11 +0800

    add prop 'mode' and 'param', add TODO waiting for detail finished

--- Vue/src/components/item-list.vue
+++ Vue/src/components/item-list.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-table :data="items" stripe @row-click="rowClickHandler" style="user-select: none">
-    <el-table-column prop="itemId" label="游戏 id" width="100px"></el-table-column>
-    <el-table-column prop="name" label="游戏名" width="500px"></el-table-column>
-    <el-table-column prop="date" label="游戏发行时间"></el-table-column>
-    <el-table-column prop="comment" label="好评度"></el-table-column>
-    <el-table-column prop="rate" label="好评率"></el-table-column>
-    <el-table-column prop="price" label="售价" width="200px"></el-table-column>
+  <el-table :data="items" stripe @row-click="rowClickHandler" style="user-select: none;">
+    <el-table-column prop="itemId" :label="titles[0]"></el-table-column>
+    <el-table-column prop="name" :label="titles[1]" width="400px"></el-table-column>
+    <el-table-column prop="date" :label="titles[2]"></el-table-column>
+    <el-table-column prop="comment" :label="titles[3]"></el-table-column>
+    <el-table-column prop="rate" :label="titles[4]" width="200px"></el-table-column>
+    <el-table-column prop="price" :label="titles[5]"></el-table-column>
   </el-table>
 </template>
 
@@ -20,15 +20,37 @@ export default {
   data() {
     return {
       detail: [],
-      items: []
+      items: [],
+      titles: ['游戏 id', '游戏名', '游戏发行时间', '好评度', '好评率', '售价'],
+      languageTitles: [
+        {
+          language: 'zh-cn',
+          titles: ['游戏 id', '游戏名', '游戏发行时间', '好评度', '好评率', '售价']
+        },
+        {
+          language: 'zh-tw',
+          titles: ['遊戲 id', '遊戲名', '遊戲發行時間', '好評度', '好評率', '售價']
+        },
+        {
+          language: 'en-us',
+          titles: ['Game id',' game name', 'Game launch time',' critical reception', 'critical reception rate',' Price']
+        }
+      ],
     }
   }, //data
   props: {
-    language: String
+    language: String,
+    mode: String,
+    param: String // param 仅在 mode 不为 all 时有效
   },
   watch: {
     language(newLanguage) {
       this.getItems(newLanguage)
+      this.languageTitles.forEach((language) => {
+        if (language.language === newLanguage) {
+          this.titles = language.titles
+        }
+      })
     }
   },
   methods: {
@@ -36,15 +58,26 @@ export default {
       this.$emit('getItem', row.itemId, row.name) // itemId, name 为后端获取的游戏的 ID 和名
     },
     getItems(language) {
-      httpGet.get('/api/' + language + '/general')
-          .then((res) => {
-            console.log(res)
-            this.items = res.data
-            this.items.pop()
-          })
-          .catch((err) => {
-            console.log(err)
-          })
+      if (this.mode === 'all') {
+        httpGet.get('/api/' + language + '/general')
+            .then((res) => {
+              this.items = res.data
+              this.items.pop()
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+      } else {
+        httpGet.get('/api' + language + '/' + this.mode + '/' + this.param)
+            .then((res) => {
+              console.log(res)
+              this.items = res.data
+              this.items.pop()
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+      }
     },
   }
 }
